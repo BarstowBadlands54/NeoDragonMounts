6f2a65226859869b5ee616f298eaac3b2ededca3
Guargamel <kingjamesxj9@gmail.com>
2026-06-26 01:03:46 +0800

    increase explosion size of fireballs

--- common/src/main/java/net/dragonmounts/neo/common/init/DragonProjectiles.java
+++ common/src/main/java/net/dragonmounts/neo/common/init/DragonProjectiles.java
@@ -5,9 +5,9 @@ import net.dragonmounts.neo.common.entity.projectile.ability.*;
 public final class DragonProjectiles {
     // minChargeTicks = how long G must be held; cooldownTicks = anti-spam after firing
     public static final DragonProjectileAbility NETHER_FIREBALL =
-            new FireballProjectileAbility(/*hold*/ 20, /*cooldown*/ 30, /*explosionPower*/ 3);
+            new FireballProjectileAbility(/*hold*/ 20, /*cooldown*/ 30, /*explosionPower*/ 5);
     public static final DragonProjectileAbility DRAGON_FIREBALL =
-            new FireballProjectileAbility(/*hold*/ 20, /*cooldown*/ 30, /*explosionPower*/ 1);
+            new FireballProjectileAbility(/*hold*/ 20, /*cooldown*/ 30, /*explosionPower*/ 3);
     public static final DragonProjectileAbility ENDER_CHARGE =
             new EnderChargeProjectileAbility(/*hold*/ 15, /*cooldown*/ 30);
 
