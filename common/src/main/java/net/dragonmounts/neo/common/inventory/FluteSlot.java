fb1552473158d93c00da8ca02dc452a51cd87d7a
Guargamel <kingjamesxj9@gmail.com>
2026-07-04 18:02:55 +0800

    removed .png

--- common/src/main/java/net/dragonmounts/neo/common/inventory/FluteSlot.java
+++ common/src/main/java/net/dragonmounts/neo/common/inventory/FluteSlot.java
@@ -18,7 +18,7 @@ import net.minecraft.world.item.ItemStack;
 import static net.dragonmounts.neo.common.DragonMountsShared.makeId;
 
 public class FluteSlot extends Slot {
-    public static final ResourceLocation ICON = makeId("item/empty_flute.png");
+    public static final ResourceLocation ICON = makeId("item/empty_flute");
     public final FluteHolder holder;
     public final DragonInventoryHandler inventory;
     public SlotListener<? super FluteSlot> listener;
